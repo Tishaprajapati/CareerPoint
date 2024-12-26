a97dd1db42f3b10b577cd1d21214ce24a1c76ddc
Tishaprajapati <134000026+Tishaprajapati@users.noreply.github.com>
2024-12-26 21:39:16 +0530

    Merging homepage changes into frontend (#5)
    * merging homepage changes
    
    * minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,36 @@
 /* eslint-disable no-unused-vars */
-//import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-//  import './App.css'
-//  export default App
-
-// function App()
-//  {
-  // eslint-disable-next-line no-undef
-  //const [count,setCount] = useState(0)
-
-//   return (
-//     <>
-//         <h1 className='text-blue-400'>hello world</h1>
-      
-//     </>
-//   )
-// }
-
 import React from 'react'
 import Navbar from './components/shared/navbar'
-import Home from './components/Home'
-// import Home from './components/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// // import { Home, LogIn } from 'lucide-react'
-import Signup from './components/auth/Signup'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
 import Login from './components/auth/Login'
-const appRouter = createBrowserRouter([
-{
-   path:'/',
-   element:<Home/>
-},
-{
-  path:'/login',
-  element:<Login/>
-},
-{
-  path:'/signup',
-  element:<Signup/>
-}
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
 
+
+const appRouter =createBrowserRouter([
+  {
+    path:'/',
+    // eslint-disable-next-line react/jsx-no-undef
+    element:<Home/>
+  },
+  {
+    path:'/login',
+     //eslint-disable-next-line react/jsx-no-undef
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    // eslint-disable-next-line react/jsx-no-undef
+    element:<Signup/>
+  },
 ])
+
 function App() {
   return (
-    <div>
-      <RouterProvider router = {appRouter}/>
-    </div>
-   
-    // <div className = "text-center text-red-400">
-    //   Hello
-    // </div>
+    <>
+    <RouterProvider router = {appRouter}/>
+    </>
   )
 }
 export default App

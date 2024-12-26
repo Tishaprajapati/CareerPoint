6ebfb662076ca09700c38de48c590198ca7832af
Tisha Prajapati <tprajapati502@gmail.com>
2024-12-26 21:38:31 +0530

    minor

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () =>
     const user = false;
     return (
         <div className='bg-white'>
-            <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+            <div className='flex items-center justify-between mx-auto max-w-6xl h-20 px-4'>
                 <div>
                     <h1 className='text-2xl font-bold'>Career<span className='text-[#F83002]'>Point</span></h1>
                 </div>
